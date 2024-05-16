d20ee6d6dd1bbe66a8a6d315be8a26a7cd9c9477
Gvantsa-Tabunidze <gvantsaletta9@gmail.com>
2024-05-16 18:18:41 +0400

    Home work tasks(Lesson1.)

---
+++ script.js
@@ -0,0 +1,31 @@
+// Task 1:
+
+let quantity = 100;
+
+if (quantity < 50) {
+  console.log('Less than 50');
+} else if (quantity > 20) {
+  console.log('More than 20');
+} else {
+  console.log('Error');
+}
+
+// Task 2:
+
+let userName = 'Mariam';
+
+let name = userName == 'Mariam' ? 'True' : 'False';
+console.log(name);
+
+// Task 3:
+
+let firstName = 'Mariam';
+
+switch (firstName) {
+  case 'Mariam':
+    console.log('True');
+    break;
+
+  default:
+    console.log('False');
+}
